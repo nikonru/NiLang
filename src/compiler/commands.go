a93a519bde8e3d31dcd5992fc21373cfbae7b399
nikonru <nikonru2000@gmail.com>
2025-01-15 16:45:13 +0300

    Add emit to compiler

---
+++ src/compiler/commands.go
@@ -0,0 +1,13 @@
+package compiler
+
+type command = string
+
+const (
+	LOAD = "ld"
+
+	// registers
+	AX = "AX"
+	BX = "BX"
+	CX = "CX"
+	DX = "DX"
+)
--- src/compiler/compiler.go
+++ src/compiler/compiler.go
@@ -4,11 +4,16 @@ import (
 	"NiLang/src/helper"
 	"NiLang/src/lexer"
 	"NiLang/src/parser"
+	"NiLang/src/ast"
+	"bytes"
+	"errors"
+	"strconv"
 	"fmt"
 	"log"
 )
 
 type Compiler struct {
+	output bytes.Buffer
 }
 
 func New() *Compiler {
@@ -24,15 +29,15 @@ func (c *Compiler) Compile(input []byte) ([]byte, error) {
 		log.Fatalf("parser.Parse() has returned nil")
 	}
 
-	errors := parser.Errors()
-	if len(errors) != 0 {
-		fmt.Printf("parser had %d error(s)\n", len(errors))
+	_errors := parser.Errors()
+	if len(_errors) != 0 {
+		fmt.Printf("parser had %d error(s)\n", len(_errors))
 		fmt.Print("parser error(s):\n")
-		for _, err := range errors {
+		for _, err := range _errors {
 			helper.PrintError(err, input)
 		}
 
-		return input, nil
+		return c.output.Bytes(), errors.New("parsing errors")
 	}
 
 	fmt.Println("PROGRAM TREE")
@@ -40,5 +45,52 @@ func (c *Compiler) Compile(input []byte) ([]byte, error) {
 		fmt.Println(statement.String())
 	}
 	fmt.Println("END")
-	return input, nil
+
+	c.emit(LOAD, AX, BX)
+	c.emit(LOAD, 2, BX)
+	c.emitLabel("lab")
+
+	return c.output.Bytes(), nil
 }
+
+func (c *Compiler) emit(op command, arg1 interface{}, arg2 interface{}) {
+
+	write := func(arg interface{}, id int){
+		switch v := arg.(type) {
+		case int:
+			c.output.WriteString(strconv.Itoa(v))
+		case string:
+			c.output.WriteString(v)
+		case bool:
+			if v{
+				c.output.WriteString("1")
+			} else{
+				c.output.WriteString("0")
+			}
+		default:
+			log.Fatalf("type of arg%d not handled. got=%T", arg, id)
+		}
+	}
+
+	c.output.WriteString(op)
+	if arg1 != nil {
+		c.output.WriteString(" ")
+		write(arg1, 1)
+
+		if arg2 != nil {
+			c.output.WriteString(" ")
+			write(arg2, 2)
+		}
+	}
+
+	c.output.WriteString("\n")
+}
+
+func (c *Compiler) emitLabel(label string){
+	c.emit(label +":", nil, nil)
+}
+
+func (c *Compiler) compileDeclarationStatement(ds *ast.DeclarationStatement){
+	return
+}
+
