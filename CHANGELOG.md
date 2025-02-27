2b243f9eff08f23f78d763dbfd9d311c06983f30
nikonru <nikonru2000@gmail.com>
2025-02-27 11:12:54 +0300

    add ci job for making release

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+The first release version of *NiLang* programming language. 
+Arithmetic operators **aren't implemented** yet.
+
+Beware, it has plenty of bugs, stack overflow errors and freezes. 
+
--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
-go build -o build/nilang  src/main.go
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    platform="linux"
+    ext=""
+elif [[ "$OSTYPE" == "msys" ]]; then
+    platform="windows"
+    ext=".exe"
+else
+    echo "ERROR: unknown platform - $OSTYPE"
+    exit
+fi
+
+go build -o build/nilang$ext  src/main.go
+tar -czvf build/nilang-$platform.tar.gz --directory=build nilang$ext
